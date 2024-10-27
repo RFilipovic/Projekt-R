19b417070295482bb140e49828bb9708aaae8bac
unknown <rene.filipovic03@gmail.com>
2024-10-27 02:09:20 +0200

    added a header file for containers implementation

--- containers.cpp
+++ containers.cpp
@@ -4,74 +4,37 @@
 #include <stdexcept>
 #include <sstream>
 
-class UntilDue
-{
-public:
-    UntilDue(int minutes, int seconds) : minutes(minutes), seconds(seconds)
-    {
-        if (minutes < 0 || seconds < 0 || seconds >= 60)
-        {
-            throw std::invalid_argument("Invalid time format");
-        }
+#include "containers.h"
+
+
+UntilDue::UntilDue(int minutes, int seconds) : minutes(minutes), seconds(seconds) {
+    if (minutes < 0 || seconds < 0 || seconds >= 60) {
+        throw std::invalid_argument("Invalid time format");
     }
+}
 
-    std::string toString() const
-    {
-        std::ostringstream oss;
-        oss << std::setw(2) << std::setfill('0') << minutes << ":"
-            << std::setw(2) << std::setfill('0') << seconds;
-        return oss.str();
-    }
+std::string UntilDue::toString() const {
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << minutes << ":"
+        << std::setw(2) << std::setfill('0') << seconds;
+    return oss.str();
+}
 
-private:
-    int minutes;
-    int seconds;
-};
+Container::Container(const std::string &id) : id(id) {}
+Container::~Container() {}
 
-class Container
-{
-public:
-    Container(const std::string &id) : id(id) {}
-    virtual ~Container() {}
-    virtual void displayDetails() const = 0;
+UntilDueContainer::UntilDueContainer(const std::string &id, const UntilDue &untilDue)
+    : Container(id), untilDue(untilDue) {}
 
-protected:
-    std::string id;
-};
 
-class UntilDueContainer : public Container
-{
-public:
-    UntilDueContainer(const std::string &id, const UntilDue &untilDue)
-        : Container(id), untilDue(untilDue) {}
+void UntilDueContainer::displayDetails() const  {
+    
+    std::cout << id << " " << untilDue.toString() << std::endl;
+}
 
-    void displayDetails() const override
-    {
-        std::cout << id << " " << untilDue.toString() << std::endl;
-    }
 
    
-    UntilDue getUntilDue() const { return untilDue; }
+UntilDue UntilDueContainer::getUntilDue() const { return untilDue; }
 
   
-    void setUntilDue(const UntilDue &newUntilDue) { untilDue = newUntilDue; }
-
-private:
-    UntilDue untilDue;
-};
-
-class PriorityContainer : public Container
-{
-public:
-    PriorityContainer(const std::string &id, int priority, const std::string &destination)
-        : Container(id), priority(priority), destination(destination) {}
-
-    void displayDetails() const override
-    {
-        std::cout << id << " " << priority << " " << destination << std::endl;
-    }
-
-private:
-    int priority;
-    std::string destination;
-};
+void UntilDueContainer::setUntilDue(const UntilDue &newUntilDue) { untilDue = newUntilDue; }
---
+++ containers.h
@@ -0,0 +1,38 @@
+#ifndef CONTAINERS_H
+#define CONTAINERS_H
+
+#include <string>
+
+class UntilDue {
+    public:
+        UntilDue(int minutes, int seconds);
+        std::string toString() const;
+
+    private:
+        int minutes;
+        int seconds;
+};
+
+class Container {
+    protected:
+        std::string id;
+
+    public:
+        Container(const std::string &id);
+        virtual ~Container();
+        virtual void displayDetails() const = 0;
+};
+
+class UntilDueContainer : public Container{
+    public:
+
+    UntilDueContainer(const std::string &id, const UntilDue &untilDue);
+    void displayDetails() const;
+    UntilDue getUntilDue() const;
+    void setUntilDue(const UntilDue &newUntilDue);
+
+private:
+    UntilDue untilDue;
+};
+
+#endif // CONTAINERS_H
