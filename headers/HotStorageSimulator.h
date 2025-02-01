90fd7d8cf5be0979326b9231f96a7de99a9d676a
Niko Žužul <117918832+NikoZuzul@users.noreply.github.com>
2025-02-01 19:36:17 +0100

    "Čitanje naredbi iz kuke iz datoteke osnovno"

--- headers/HotStorageSimulator.h
+++ headers/HotStorageSimulator.h
@@ -4,16 +4,22 @@
 #include "SingleContainerCrane.h"
 #include "Printer.h"
 #include "ParsedBuffers.h"
+#include <fstream> // Za rad s datotekama
 
-class HotStorageSimulator {
+class HotStorageSimulator
+{
 public:
     HotStorageSimulator(Printer &p);
     void simulate();
+
 private:
     Printer *printer;
     void runEntryStack();
     void runCrane();
     void runOutgoingStack();
+
+    // Funkcija za učitavanje naredbi iz datoteke
+    bool getNextCommandFromFile(std::ifstream &file, int &input1, int &input2);
 };
 
-#endif //SIMULATOR_H
+#endif // SIMULATOR_H
--- src/hot-storage/HotStorageSimulator.cpp
+++ src/hot-storage/HotStorageSimulator.cpp
@@ -5,16 +5,26 @@
 #include "EntryContainerStack.h"
 #include "OutGoingContainerStack.h"
 
-HotStorageSimulator::HotStorageSimulator(Printer &p){
-        *this->printer = p;
+HotStorageSimulator::HotStorageSimulator(Printer &p)
+{
+    *this->printer = p;
 }
 
-//dretvene funkcije
-//pokrece dolazni stog paralelno
-void HotStorageSimulator::runEntryStack(){
+// Učitavanje naredbi iz datoteke
+bool HotStorageSimulator::getNextCommandFromFile(std::ifstream &file, int &input1, int &input2)
+{
+    if (file >> input1 >> input2)
+    {
+        return true;
+    }
+    return false;
+}
 
+// pokreće dolazni stog paralelno
+void HotStorageSimulator::runEntryStack()
+{
     auto data = printer->getParsedBuffers();
-    EntryContainerStack *entryStack = dynamic_cast<EntryContainerStack*>(data->getBuffers().at(0));
+    EntryContainerStack *entryStack = dynamic_cast<EntryContainerStack *>(data->getBuffers().at(0));
 
     while (1)
     {
@@ -22,62 +32,78 @@ void HotStorageSimulator::runEntryStack(){
     }
 }
 
-//pokrece kuku paralelno
-void HotStorageSimulator::runCrane(){
+// pokreće kuku paralelno
+void HotStorageSimulator::runCrane()
+{
     printer->printEverything();
 
     auto data = printer->getParsedBuffers();
-    SingleContainerCrane *crane = dynamic_cast<SingleContainerCrane*>(printer->getCrane());
-    EntryContainerStack *entryStack = dynamic_cast<EntryContainerStack*>(data->getBuffers().at(0));
-    OutGoingContainerStack *outgoingStack = dynamic_cast<OutGoingContainerStack*>(data->getBuffers().at(4));
+    SingleContainerCrane *crane = dynamic_cast<SingleContainerCrane *>(printer->getCrane());
+    EntryContainerStack *entryStack = dynamic_cast<EntryContainerStack *>(data->getBuffers().at(0));
+    OutGoingContainerStack *outgoingStack = dynamic_cast<OutGoingContainerStack *>(data->getBuffers().at(4));
 
     UntilDue lower = data->getCraneLower();
     UntilDue move = data->getCraneMove();
     UntilDue lift = data->getCraneLift();
 
-    while(1){
+    std::ifstream commandFile("comands.txt"); // Otvori datoteku s naredbama
+    if (!commandFile)
+    {
+        std::cerr << "Ne mogu otvoriti datoteku naredbi!" << std::endl;
+        return;
+    }
 
+    while (1)
+    {
         int input1, input2;
-        std::cout<<"Upisite indeks stoga sa kojeg zelite uzeti kontejner: ";
-        std::cin>>input1;
-        std::cout<<"Upisite indeks stoga na koji zelite staviti kontejner: ";
-        std::cin>>input2;
 
-        if(input1 == input2){
-            std::cout<<"Ne mozete premjestiti kontejner na isti stog!"<<std::endl;
+        if (!getNextCommandFromFile(commandFile, input1, input2))
+        {
+            std::cout << "Nema više naredbi u datoteci. Prekidam simulaciju." << std::endl;
+            break;
+        }
+
+        if (input1 == input2)
+        {
+            std::cout << "Ne mozete premjestiti kontejner na isti stog!" << std::endl;
             continue;
-        }else if(input2 == 0){
-            std::cout<<"Ne mozete stavljati kontejnere na dolazni stog!"<<std::endl;
+        }
+        else if (input2 == 0)
+        {
+            std::cout << "Ne mozete stavljati kontejnere na dolazni stog!" << std::endl;
             continue;
-        }else if(input1 == 4){
-            std::cout<<"Ne mozete uzimati kontejnere sa odlaznog stoga!"<<std::endl;
+        }
+        else if (input1 == 4)
+        {
+            std::cout << "Ne mozete uzimati kontejnere sa odlaznog stoga!" << std::endl;
             continue;
         }
 
         entryStack->continueTime();
         outgoingStack->continueTime();
-        
-        if(input1 != crane->getAboveStackIndex()){
+
+        if (input1 != crane->getAboveStackIndex())
+        {
             data->refreshTime(move);
             sleep(move.getMinutes() * 60 + move.getSeconds());
             crane->setAboveStackIndex(input1);
-            std::cout<<"Kuka je pomaknuta iznad stoga "<< crane->getAboveStack() <<std::endl;
+            std::cout << "Kuka je pomaknuta iznad stoga " << crane->getAboveStack() << std::endl;
             printer->printEverything();
         }
 
         data->refreshTime(lower);
-        std::cout<<"Kuka se spusta."<<std::endl;
+        std::cout << "Kuka se spusta." << std::endl;
         sleep(lower.getMinutes() * 60 + lower.getSeconds());
-        std::cout<<"Kuka se spustila, skupila kontejner i pocela se dizat."<<std::endl;
+        std::cout << "Kuka se spustila, skupila kontejner i pocela se dizat." << std::endl;
 
-        UntilDueContainer *container = static_cast<UntilDueContainer*>(data->getBuffers().at(input1)->pop());
+        UntilDueContainer *container = static_cast<UntilDueContainer *>(data->getBuffers().at(input1)->pop());
         crane->setHookContent(container);
 
         data->refreshTime(lift);
         sleep(lift.getMinutes() * 60 + lift.getSeconds());
         crane->refreshTime(lift);
 
-        std::cout<<"Podignut je kontejner "<< container->getDetails()<<std::endl;
+        std::cout << "Podignut je kontejner " << container->getDetails() << std::endl;
 
         printer->printEverything();
 
@@ -86,37 +112,39 @@ void HotStorageSimulator::runCrane(){
         data->refreshTime(move);
         crane->refreshTime(move);
         sleep(move.getMinutes() * 60 + move.getSeconds());
-        std::cout<<"Kuka je pomaknuta iznad stoga "<< crane->getAboveStack() <<std::endl;
+        std::cout << "Kuka je pomaknuta iznad stoga " << crane->getAboveStack() << std::endl;
         printer->printEverything();
 
         data->refreshTime(lower);
         crane->refreshTime(lower);
-        std::cout<<"Kuka se spusta."<<std::endl;
+        std::cout << "Kuka se spusta." << std::endl;
         sleep(lower.getMinutes() * 60 + lower.getSeconds());
 
-        if(input2 != 4)
+        if (input2 != 4)
             data->getBuffers().at(input2)->push(*container);
         else
             data->getBuffers().at(input2)->push_infront(*container);
 
-        std::cout<<"Kuka je ostavila kontejner na stog i pocela se dizat."<<std::endl;
+        std::cout << "Kuka je ostavila kontejner na stog i pocela se dizat." << std::endl;
         printer->printEverything();
 
         data->refreshTime(lift);
         sleep(lift.getMinutes() * 60 + lift.getSeconds());
-        std::cout<<"Kuka spremna za iducu naredbu —>"<<std::endl;
+        std::cout << "Kuka spremna za iducu naredbu —>" << std::endl;
         printer->printEverything();
 
         entryStack->pauseTime();
         outgoingStack->pauseTime();
     }
+
+    commandFile.close(); // Zatvori datoteku
 }
 
-//pokrece odlazni stog paralelno
-void HotStorageSimulator::runOutgoingStack(){
-
+// pokreće odlazni stog paralelno
+void HotStorageSimulator::runOutgoingStack()
+{
     auto data = printer->getParsedBuffers();
-    OutGoingContainerStack *outgoingStack = dynamic_cast<OutGoingContainerStack*>(data->getBuffers().at(4));
+    OutGoingContainerStack *outgoingStack = dynamic_cast<OutGoingContainerStack *>(data->getBuffers().at(4));
 
     while (1)
     {
@@ -124,13 +152,15 @@ void HotStorageSimulator::runOutgoingStack(){
     }
 }
 
-void HotStorageSimulator::simulate(){
-    
-    std::thread entryStack([this]() { this->runEntryStack(); });
-    std::thread crane([this]() { this->runCrane(); });
-    std::thread outgoingStack([this]() { this->runOutgoingStack(); });
+void HotStorageSimulator::simulate()
+{
+    std::thread entryStack([this]()
+                           { this->runEntryStack(); });
+    std::thread crane([this]()
+                      { this->runCrane(); });
+    std::thread outgoingStack([this]()
+                              { this->runOutgoingStack(); });
 
     entryStack.join();
     crane.join();
-    
 }
